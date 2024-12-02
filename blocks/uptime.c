0de01c340c8c42a5eedf840f6e7b100a1a66e7c9
peajack <peajackboatan@ya.ru>
2024-12-02 19:33:12 +0700

    continuing

--- blocks/uptime.c
+++ blocks/uptime.c
@@ -1,8 +1,6 @@
-#ifdef __linux__
-#define _POSIX_C_SOURCE 199309L
-#endif
-
+#ifndef __linux__
 #include <time.h>
+#endif
 #include <stdio.h>
 #include "uptime.h"
 #include "../util.h"
@@ -12,17 +10,30 @@ void *uptime(void *args) {
     data->label = "UPTIME";
     data->result = "n/a";
 
+    int days, hours, minutes = 0;
+    long long sec = 0;
+#if defined(__linux__)
+
+    FILE *uptime_file = fopen("/proc/uptime", "r");
+    char line[100];
+    fgets(line, 100, uptime_file);
+    sscanf(line, "%lld %[]", sec);
+    fclose(uptime_file);
+
+#else
+
     struct timespec timespec;
     clock_gettime(CLOCK_UPTIME, &timespec);
+    sec = timespec.tv_sec;
 
-    long long sec = timespec.tv_sec;
-    int days, hours, minutes = 0;
+#endif
+
     days = sec / 86400;
     sec = sec % 86400;
     hours = sec / 3600;
     sec = sec % 3600;
     minutes = sec / 60;
-
+    
     char buf[50];
     snprintf(buf, 50, "%dd %dh %dm", days, hours, minutes);
     data->result = buf;
